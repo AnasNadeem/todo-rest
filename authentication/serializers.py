69733ca74786a657fd3c42bd515aba8fde26a064
Anas Nadeem <anas5678go@gmail.com>
2022-12-04 17:20:39 +0530

    Added id in userserializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'date_joined')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date_joined')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
